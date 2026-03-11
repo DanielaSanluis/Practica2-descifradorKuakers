f163fbe09d969b7fe381d4f1e0dd78140ab09e69
jzarcoo <zarco_romero_jose_antonio@comunidad.unam.mx>
2026-03-10 20:37:10 -0600

    feat: entregable

--- buscarLlaves.py
+++ buscarLlaves.py
@@ -1,191 +1,300 @@
-import sys
+from math import gcd
 import os
 import descifrador
 
 # Carpeta donde se guardará todo
 OUTPUT_DIR = "resultados_descifrado"
+# Como 256 es una potencia de 2, los números coprimos con 256 son exactamente los números impares entre 1 y 255.
+COPRIMOS = list(range(1, 256, 2))     
+# Lista de archivos a procesar y sus algoritmos correspondientes (si se quiere probar solo una llave específica por algoritmo)
+ARCHIVOS = ["file1.lol", "file2.lol", "file3.lol", "file4.lol"]
+ALGORITMOS = ["cesar", "decimado", "afin", "base64"]
 
 
-"""Crear el directorio de salida si no existe"""
 def preparar_directorio():
+    """
+    Prepara el directorio de salida para guardar los resultados del descifrado.
+    Si el directorio no existe, lo crea.
+    """
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
 
 
-"""Guarda los bytes descifrados en un archivo binario"""
 def guardar_resultado(nombre_archivo, algoritmo, llave_info, datos):
+    """
+    Guarda el resultado del descifrado en un archivo dentro del directorio de salida.
+    Args:
+    nombre_archivo (str): El nombre base del archivo original sin extensión.
+    algoritmo (str): El nombre del algoritmo utilizado para el descifrado.
+    llave_info (str): Información sobre la llave utilizada.
+    datos (bytes): Los datos descifrados que se desean guardar en el archivo.
+    """
     nombre = f"{nombre_archivo}_{algoritmo}_{llave_info}.bin"
     ruta = os.path.join(OUTPUT_DIR, nombre)
 
     with open(ruta, "wb") as f:
         f.write(datos)
 
-"""Verifica los bytes de los archivos más comunes. Regresamos True si encuentra una coincidencia"""
-def verificar_firma(datos):
-
-    # -------- Imagen --------
-    
+def imagen(datos):
+    """
+    Verifica si los datos descifrados contienen una firma de formato de archivo de imagen común (PNG, JPG, BMP).
+    Args:
+        datos (bytes): Los datos descifrados que se desean verificar.
+    Returns:
+        bool: True si se detecta una firma de formato de archivo de imagen común, False en caso contrario.
+    """
     # PNG : 89 50 4E 47 0D 0A 1A 0A
     if datos.startswith(bytes.fromhex("89504E470D0A1A0A")):
         return True
-
     # JPG : FF D8 FF + [DB | EE | E1]
     if len(datos) >= 4 and datos[0:3] == bytes.fromhex("FFD8FF") and datos[3] in [0xDB,0xEE,0xE1]:
         return True
-
     # BMP : 42 4D
     if datos.startswith(bytes.fromhex("424D")):
         return True
+    return False
 
-
-    # -------- Audio --------
-
+def audio(datos):
+    """
+    Verifica si los datos descifrados contienen una firma de formato de archivo de audio común (MP3, WAV, OGG).
+    Args:
+        datos (bytes): Los datos descifrados que se desean verificar.
+    Returns:
+        bool: True si se detecta una firma de formato de archivo de audio común, False en otro caso.
+    """
     # MP3 : FF FB, FF F3, FF F2, 49 44 33
     if datos.startswith(bytes.fromhex("FFFB")):
         return True
-
     if datos.startswith(bytes.fromhex("FFF3")):
         return True
-
     if datos.startswith(bytes.fromhex("FFF2")):
         return True
-
     if datos.startswith(bytes.fromhex("494433")):
         return True
-
     # WAV : 52 49 46 46 ?? ?? ?? ?? 57 41 56 45
     if len(datos) >= 12 and datos[0:4] == bytes.fromhex("52494646") and datos[8:12] == bytes.fromhex("57415645"):
         return True
-
     # OGG : 4F 67 67 53
     if datos.startswith(bytes.fromhex("4F676753")):
         return True
+    return False
 
-
-    # -------- Video --------
-
+def video(datos):
+    """
+    Verifica si los datos descifrados contienen una firma de formato de archivo de video común (MP4, AVI, MKV).
+    Args:
+        datos (bytes): Los datos descifrados que se desean verificar.
+    Returns:
+        bool: True si se detecta una firma de formato de archivo de video común, False en otro caso.
+    """
     # MP4 : 66 74 79 70 [ 69 73 6F 6D | 4D 53 4E 56 ]
     if len(datos) >= 12 and datos[4:8] == bytes.fromhex("66747970"):
         if datos[8:12] == bytes.fromhex("69736F6D") or datos[8:12] == bytes.fromhex("4D534E56"):
             return True
-
     # AVI : 52 49 46 46 ?? ?? ?? ?? 41 56 49 20
     if len(datos) >= 12 and datos[0:4] == bytes.fromhex("52494646") and datos[8:12] == bytes.fromhex("41564920"):
         return True
-
     # MKV : 1A 45 DF A3
     if datos.startswith(bytes.fromhex("1A45DFA3")):
         return True
+    return False
 
-     # -------- Documento --------
-
+def documento(datos):
+    """
+    Verifica si los datos descifrados contienen una firma de formato de archivo de documento común (DOCX, EPUB, PDF).
+    Args:
+        datos (bytes): Los datos descifrados que se desean verificar.
+    Returns:
+        bool: True si se detecta una firma de formato de archivo de documento común, False en otro caso.
+    """
     # DOCX / EPUB
     if datos.startswith(bytes.fromhex("504B0304")):
         return True
-
     if datos.startswith(bytes.fromhex("504B0506")):
         return True
-
     if datos.startswith(bytes.fromhex("504B0708")):
         return True
-
     # PDF : 25 50 44 46 2D
     if datos.startswith(bytes.fromhex("255044462D")):
         return True
-
-
     return False
 
+def verificar_firma(datos):
+    """
+    Verifica si los datos descifrados contienen una firma de formato de archivo común.
+    Se revisan las firmas de archivos de imagen, audio, video y documentos.
+    Args:
+    datos (bytes): Los datos descifrados que se desean verificar.
+    Returns:
+    bool: True si se detecta una firma de formato de archivo común, False en caso contrario.
+    """    
+    return imagen(datos) or audio(datos) or video(datos) or documento(datos)
 
-""" 1 """
-def buscar_cesar(datos, nombre_archivo):
+
+def buscar_cesar(datos, nombre_archivo, brute_force=True):
+    """
+    Realiza un ataque de fuerza bruta al cifrado César probando todas las posibles llaves (0-255) y guarda los resultados. 
+    Args:    
+        datos (bytes): Los datos a descifrar.
+        nombre_archivo (str): El nombre base del archivo original sin extensión para guardar los resultados
+        brute_force (bool): Si es True, se probarán todas las llaves entre 0 y 255. Si es False, se probará una llave específica.
+    """
 
     print("Buscando llaves para César...")
 
-    for llave in range(256):
+    if not brute_force:
+        llave = 77
         resultado = descifrador.procesar_cesar(datos, llave, "descifrar")
-        
+        guardar_resultado(nombre_archivo, "cesar", f"key{llave}", resultado)
         if verificar_firma(resultado):
             print("Firma detectada con llave:", llave)
-            guardar_resultado(nombre_archivo, "cesar", f"key{llave}", resultado)
-            # No se si esto sea válido. Lo pongo para salir del ciclo en cuanto se encuentre
-            # una firma válida.
             return True
+        return False
+
+    for llave in range(256):
+        print(f"Probando llave: {llave}", end="\r")
+        resultado = descifrador.procesar_cesar(datos, llave, "descifrar")
+        if verificar_firma(resultado):
+            guardar_resultado(nombre_archivo, "cesar", f"key{llave}", resultado)
+            print("Firma detectada con llave:", llave)
+            return True
+    return False
 
 
-""" 2 """
-def buscar_decimado(datos, nombre_archivo):
+def buscar_decimado(datos, nombre_archivo, brute_force=True):
+    """
+    Realiza un ataque de fuerza bruta al cifrado decimado probando todas las posibles llaves (números impares entre 1 y 255) y guarda los resultados.
+    Args:    
+        datos (bytes): Los datos a descifrar.
+        nombre_archivo (str): El nombre base del archivo original sin extensión para guardar los resultados
+        brute_force (bool): Si es True, se probarán todas las llaves impares entre 1 y 255. Si es False, se probará una llave específica (alfa=27).
+    """
 
     print("Buscando llaves para Decimado...")
 
-    for alpha in range(1, 256):
+    if not brute_force:
+        alpha = 27
+        resultado = descifrador.procesar_decimado(datos, alpha, "descifrar")
+        if verificar_firma(resultado):
+            guardar_resultado(nombre_archivo, "decimado", f"alpha{alpha}", resultado)
+            print("Firma detectada con alpha:", alpha)
+            return True
+        return False
+
+    for alpha in COPRIMOS:
         try:
+            print(f"Probando alpha: {alpha}", end="\r")
             resultado = descifrador.procesar_decimado(datos, alpha, "descifrar")
-
             if verificar_firma(resultado):
-                print("Firma detectada con alpha:", alpha)
                 guardar_resultado(nombre_archivo, "decimado", f"alpha{alpha}", resultado)
+                print("Firma detectada con alpha:", alpha)
                 return True
-
         except:
             pass
+    return False
 
 
-""" 3 """
-def buscar_afin(datos, nombre_archivo):
-
+def buscar_afin(datos, nombre_archivo, brute_force=True):
+    """
+    Realiza un ataque de fuerza bruta al cifrado afín probando todas las posibles combinaciones de llaves (alfa y beta) y guarda los resultados.
+    Args:    
+        datos (bytes): Los datos a descifrar.
+        nombre_archivo (str): El nombre base del archivo original sin extensión para guardar los resultados
+        brute_force (bool): Si es True, se probarán todas las combinaciones de alfa y beta. Si es False, se probará una combinación específica de alfa=99 y beta=99.
+    """
     print("Buscando llaves para Afín...")
-    #inicio =  141
-    for alpha in range(1, 256):
-        for beta in range(256):
+    if not brute_force:
+        alpha = 99
+        beta = 99
+        resultado = descifrador.procesar_afin(datos, alpha, beta, "descifrar")
+        if verificar_firma(resultado):
+            guardar_resultado(nombre_archivo, "afin", f"alpha{alpha}_beta{beta}", resultado)
+            print("Firma detectada con alpha:", alpha, "beta:", beta)
+            return True
+        return False
+    
+    inicio =  0
+    for alpha in COPRIMOS:
+        for beta in range(inicio, 256):
             try:
+                print(f"Probando alpha: {alpha}, beta: {beta}", end="\r")
                 resultado = descifrador.procesar_afin(datos, alpha, beta, "descifrar")
-
                 if verificar_firma(resultado):
+                    guardar_resultado(
+                        nombre_archivo,
+                        "afin",
+                        f"alpha{alpha}_beta{beta}",
+                        resultado
+                    )
                     print("Firma detectada con alpha:", alpha, "beta:", beta)
-                    guardar_resultado(nombre_archivo, "afin", f"alpha{alpha}_beta{beta}", resultado)
                     return True
             except:
                 pass
-        #inicio = 0
+        inicio = 0
+    return False
 
+def buscar_base64(datos, nombre_archivo):
+    """
+    Intenta descifrar los datos utilizando el algoritmo Base64 y guarda el resultado.
+    Args:    
+        datos (bytes): Los datos a descifrar.
+        nombre_archivo (str): El nombre base del archivo original sin extensión para guardar los resultados
+    """
 
-"""Donde la magia ocurre..."""
+    print("Buscando llaves para Base64...")
+
+    resultado = descifrador.procesar_base64(datos, "descifrar")
+
+    guardar_resultado(nombre_archivo, "base64", "", resultado)
+    print("Firma detectada con Base64")
+    return True
+
+def probar_algoritmos(datos, nombre_archivo, brute_force=True):
+    """
+    Prueba todos los algoritmos de cifrado (César, Decimado, Afín y Base64) para descifrar los datos y guarda los resultados.
+    Args:    
+        datos (bytes): Los datos a descifrar.
+        nombre_archivo (str): El nombre base del archivo original sin extensión para guardar los resultados
+        brute_force (bool): Si es True, se probarán todas las llaves posibles para cada algoritmo. Si es False, se probará una llave específica para cada algoritmo.
+    """
+    if buscar_cesar(datos, nombre_archivo, brute_force):
+        return
+    if buscar_decimado(datos, nombre_archivo, brute_force):
+        return
+    if buscar_afin(datos, nombre_archivo, brute_force):
+        return
+    if buscar_base64(datos, nombre_archivo):
+        return
+
 def main():
 
     preparar_directorio()
 
-    archivos = ["file1.lol", "file2.lol", "file3.lol", "file4.lol"]
-    #archivos = [ "file2.lol", "file3.lol", "file4.lol"]
-    #archivos = [  "file3.lol"]
-    algoritmos = ["cesar", "decimado", "afin"]
-    #algoritmos = ["afin"]
-    
-    for archivo in archivos:
-        if not os.path.exists(archivo):
-            print(f"Advertencia: No se encontró el archivo {archivo}")
-            continue
+    fuerza_bruta = False  # Cambia a False para probar las llaves especificas.
 
-        print("\n============================")
-        print("Probando archivo:", archivo)
-        print("============================")
 
-        with open(archivo, "rb") as f:
-            datos = f.read()
+    if fuerza_bruta:
+        for archivo in ARCHIVOS:
+            print(f"\nProcesando {archivo}...")
+            with open(archivo, "rb") as f:
+                datos = f.read()
+                nombre_archivo = os.path.splitext(archivo)[0]
+                probar_algoritmos(datos, nombre_archivo, brute_force=True)
+    else:
+        for archivo, algoritmo in zip(ARCHIVOS, ALGORITMOS):
+            print(f"\nProcesando {archivo} con {algoritmo}...")
+            with open(archivo, "rb") as f:
+                datos = f.read()
+                nombre_archivo = os.path.splitext(archivo)[0]
+                if algoritmo == "cesar":
+                    buscar_cesar(datos, nombre_archivo, brute_force=False)
+                elif algoritmo == "decimado":
+                    buscar_decimado(datos, nombre_archivo, brute_force=False)
+                elif algoritmo == "afin":
+                    buscar_afin(datos, nombre_archivo, brute_force=False)
+                elif algoritmo == "base64":
+                    buscar_base64(datos, nombre_archivo)
 
-        nombre_base = os.path.splitext(os.path.basename(archivo))[0]
-
-        for algoritmo in algoritmos:
-
-            print("\nProbando algoritmo:", algoritmo)
-            if algoritmo == "cesar":
-                buscar_cesar(datos, nombre_base)
-
-            if algoritmo == "decimado":
-                buscar_decimado(datos, nombre_base)
-
-            if algoritmo == "afin":
-                buscar_afin(datos, nombre_base)
 
 
 if __name__ == "__main__":
--- descifrador.py
+++ descifrador.py
@@ -1,24 +1,63 @@
 import sys
 
-"""Calcula el inverso multiplicativo"""
-def calcula_inverso(a, n = 256):
-    for x in range(n):
-        if (a * x) % n == 1:
-            return x
-            
-    return None
+def mcd(a, b):
+    """
+    Calcula el máximo común divisor de a y b, así como los coeficientes x, y que satisfacen la ecuación ax + by = gcd(a, b)
+    Args:
+        a (int): El primer número.
+        b (int): El segundo número.
+    Returns:
+        tuple: Una tupla que contiene el máximo común divisor, el coeficiente x y el coeficiente y.
+    """
+    if b == 0:
+        return a, 1, 0
+    gcd, x1, y1 = mcd(b, a % b)
+    x = y1
+    y = x1 - (a // b) * y1
+    return gcd, x, y
 
+def calcula_inverso(a, n=256):
+    """
+    Calcula el inverso multiplicativo de a módulo n utilizando el algoritmo de Euclides extendido.
+    Args:
+    a (int): El número del cual se desea calcular el inverso multiplicativo.
+    n (int): El módulo, por defecto es 256.
+    Returns:
+    int: El inverso multiplicativo de a módulo n, o None si no existe.
+    """
+    gcd, x, _ = mcd(a, n)
+    if gcd != 1:
+        return None 
+    else:
+        return (x % n + n) % n  # Asegura que el resultado sea positivo
 
-"""Aplicar el descifrado o cifrado César"""
-def procesar_cesar(datos, llave, modo = 'descifrar'):
+def procesar_cesar(datos, llave, modo='descifrar'):
+    """
+    Aplica el cifrado César a los datos utilizando la llave proporcionada con n=256.
+    Args:    
+        datos (bytes): Los datos a cifrar o descifrar.
+        llave (int): La cantidad de desplazamiento para el cifrado César.
+        modo (str): El modo de operación, puede ser 'cifrar' o 'descifrar'. Por defecto es 'descifrar'.
+    Returns:    
+        bytes: Los datos procesados después de aplicar el cifrado César.
+    """
     if modo == 'descifrar':
         return bytes([(b - llave) % 256 for b in datos])
         
     return bytes([(b + llave) % 256 for b in datos])
 
-
-"""Aplicar el descifrado o cifrado Decimado. 'alfa' debe ser coprimo con 256"""
-def procesar_decimado(datos, alfa, modo = 'descifrar'):
+def procesar_decimado(datos, alfa, modo='descifrar'):
+    """"
+    Aplica el cifrado decimado a los datos utilizando la llave proporcionada con n=256.
+    Args:
+        datos (bytes): Los datos a cifrar o descifrar.
+        alfa (int): La llave para el cifrado decimado, debe ser un número impar entre 1 y 255.
+        modo (str): El modo de operación, puede ser 'cifrar' o 'descifrar'. Por defecto es 'descifrar'.
+    Returns:
+        bytes: Los datos procesados después de aplicar el cifrado decimado.
+    Raises:
+        Exception: Si alfa no es un número impar entre 1 y 255.
+    """
     lista_temporal = []
     # Obtenemos el inverso multiplicativo.
     inverso = calcula_inverso(alfa) 
@@ -43,6 +82,21 @@ def procesar_decimado(datos, alfa, modo = 'descifrar'):
 
     return bytes(lista_temporal)
 
+def procesar_afin(datos, alfa, beta, modo='descifrar'):
+    """"
+    Aplica el cifrado afín a los datos utilizando las llaves proporcionadas.
+    Args:
+        datos (bytes): Los datos a cifrar o descifrar.
+        alfa (int): La llave multiplicativa para el cifrado afín, debe ser un número impar entre 1 y 255.
+        beta (int): La llave aditiva para el cifrado afín, debe ser un número entre 0 y 255.
+        modo (str): El modo de operación, puede ser 'cifrar' o 'descifrar'. Por defecto es 'descifrar'.
+    Returns:        
+        bytes: Los datos procesados después de aplicar el cifrado afín.
+    Raises:        
+        Exception: Si alfa no es un número impar entre 1 y 255, o si beta no es un número entre 0 y 255.
+    """
+    # 1. Creamos una lista vacia para guardar los bytes procesados
+    lista_resultado = []
 
 """Aplicar el descifrado o cifrado Afin"""
 def procesar_afin(datos, alfa, beta, modo = 'descifrar'):
@@ -54,7 +108,10 @@ def procesar_afin(datos, alfa, beta, modo = 'descifrar'):
     # Detenemos el programa si alfa no tiene inverso modulo 256
     if inverso is None:
         # Usamos Exception genérica como pediste
-        raise Exception("Error: alfa no tiene inverso módulo 256.")
+        raise Exception("Error: El valor de alfa no es valido (debe ser impar).")
+    
+    if beta < 0 or beta > 255:
+        raise Exception("Error: El valor de beta no es valido (debe estar entre 0 y 255).")
 
     if modo == 'descifrar':
         for byte_cifrado in datos:
@@ -76,13 +133,17 @@ def procesar_afin(datos, alfa, beta, modo = 'descifrar'):
             
     return bytes(lista_resultado)
 
-
-"""Codifica o decodifica datos en Base64"""
-def procesar_base64(datos, modo = 'cifrar'):
+def procesar_base64(datos, modo='cifrar'):
+    """
+    Aplica el cifrado o descifrado Base64 a los datos.
+    Args:
+        datos (bytes): Los datos a cifrar o descifrar.
+        modo (str): El modo de operación, puede ser 'cifrar' o 'descifrar'. Por defecto es 'cifrar'.
+    Returns:
+        bytes: Los datos procesados después de aplicar el cifrado o descifrado Base64
+    """
     chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-    
     if modo == 'cifrar':
-    
         cadena_binaria_total = ""
         for byte in datos:
             # Convertimos el numero a binario, quitamos el prefijo '0b' 
@@ -101,10 +162,8 @@ def procesar_base64(datos, modo = 'cifrar'):
         # Cada simbolo Base64 representa 6 bits 
         for i in range(relleno_necesario * 2):
             cadena_binaria_total = cadena_binaria_total + "0"
-            
         resultado_texto = ""
         indice = 0
-        
         while indice < len(cadena_binaria_total):
             bloque_6_bits = cadena_binaria_total[indice : indice + 6]
             while len(bloque_6_bits) < 6:
@@ -112,47 +171,51 @@ def procesar_base64(datos, modo = 'cifrar'):
             valor_decimal = int(bloque_6_bits, 2)
             # Usamos el valor decimal como indice para buscar el caracter en la cadena 'chars'
             caracter_base64 = chars[valor_decimal]
-            resultado_texto = resultado_texto + caracter_base64            
+            resultado_texto = resultado_texto + caracter_base64
+            
             # Saltamos al siguiente bloque de 6
             indice = indice + 6
 
         for i in range(relleno_necesario):
             resultado_texto = resultado_texto + "=" 
-            
-        # El fin...
         return resultado_texto.encode()
-        
-        
     else:  # descifrar
 
         texto =  datos.decode("ascii")
-        # No estaría mal limpiar los saltos de línea y regresos en la misma antes de procesar.
-        texto = texto.replace("\n", "").replace("\r", "").rstrip("=")
+
+        texto = texto.rstrip("=")
+
         cadena_binaria_total = ""
 
         for c in texto:
             valor = chars.index(c)
+
             binario_6 = bin(valor)[2:]
+
             while len(binario_6) < 6:
                 binario_6 = "0" + binario_6
 
             cadena_binaria_total += binario_6
 
         resultado_bytes = []
+
         indice = 0
 
         while indice + 8 <= len(cadena_binaria_total):
+
             bloque_8 = cadena_binaria_total[indice:indice+8]
+
             valor = int(bloque_8, 2)
+
             resultado_bytes.append(valor)
+
             indice += 8
 
         return bytes(resultado_bytes)
 
 
-"""Donde la magia ocurre..."""
 def main():
-
+    """Función principal que maneja la lógica de cifrado y descifrado basada en los argumentos de línea de comandos."""
     if len(sys.argv) < 4:
         print("Uso: python descifrador.py <modo: cifrar/descifrar> <algoritmo> <archivo> [llaves]")
         print("Algoritmos: cesar, decimado, afin, base64")
